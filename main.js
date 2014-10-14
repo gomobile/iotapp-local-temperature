56f50b2372b5749c2e52f525f808d4f469862060
eashtian <eashtian@EASHTIAN-MOBL1.amr.corp.intel.com>
2014-10-14 10:26:50 -0700

    add project files

--- main.js
+++ main.js
@@ -1,8 +1,50 @@
-//GROVE Kit A0 --> Aio(0)
+/*jslint node:true,vars:true,bitwise:true,unparam:true */
+
+/*jshint unused:true */
+
+/*
+The Local Temperature Node.js sample application distributed within Intel® XDK IoT Edition under the IoT with Node.js Projects project creation option showcases how to read analog data from a Grover Starter Kit Plus – IoT Intel® Edition Temperature Sensor, start a web server and communicate wirelessly using WebSockets.
+
+MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
+Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with port nanmes/numbering that match boards & with bindings to javascript & python.
+
+Steps for installing MRAA & UPM Library on Intel IoT Platform with IoTDevKit Linux* image
+Using a ssh client: 
+1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
+2. opkg update
+3. opkg upgrade
+
+Article: https://software.intel.com/en-us/html5/articles/iot-local-temperature-nodejs-and-html5-samples
+*/
+
 var B = 3975;
 var mraa = require("mraa");
 
+//GROVE Kit A0 --> Aio(0)
 var myAnalogPin = new mraa.Aio(0);
+
+/*
+Function: startSensorWatch(socket)
+Parameters: socket - client communication channel
+Description: Read Temperature Sensor and send temperature in degrees of Fahrenheit every 4 seconds
+*/
+function startSensorWatch(socket) {
+    'use strict';
+    setInterval(function () {
+        var a = myAnalogPin.read() >> 2;
+        //console.log("Checking....");
+
+        //console.log("Analog Pin (A0) Output: " + a);
+        var resistance = (1023 - a) * 10000 / a; //get the resistance of the sensor;
+        //console.log("Resistance: "+resistance);
+        var celsius_temperature = 1 / (Math.log(resistance / 10000) / B + 1 / 298.15) - 273.15;//convert to temperature via datasheet ;
+        //console.log("Celsius Temperature "+celsius_temperature); 
+        var fahrenheit_temperature = (celsius_temperature * (9 / 5)) + 35;
+        //console.log("Fahrenheit Temperature "+ fahrenheit_temperature);
+        socket.emit("message", fahrenheit_temperature);
+    }, 4000);
+}
+
 console.log("Sample Reading Temperature Sensor");
 
 //Create Socket.io server
@@ -10,42 +52,28 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world from Intel Galileo</h1>');
+app.get('/', function (req, res) {
+    'use strict';
+    res.send('<h1>Hello world from Intel Galileo</h1>');
 });
 
 //Attach a 'connection' event handler to the server
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
+    'use strict';
     console.log('a user connected');
     //Emits an event along with a message
     socket.emit('connected', 'Welcome');
-    
+
     //Start watching Sensors connected to Galileo board
     startSensorWatch(socket);
-    
+
     //Attach a 'disconnect' event handler to the socket
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 });
 
-http.listen(1337, function(){
-  console.log('listening on *:1337');
+http.listen(1337, function () {
+    'use strict';
+    console.log('listening on *:1337');
 });
-
-//---------------------------------------------------------------------------
-function startSensorWatch(socket){
-    setInterval(function(){
-        var a = myAnalogPin.read() >> 2;
-        //console.log("Checking....");
-
-        //console.log("Analog Pin (A0) Output: " + a);
-        var resistance = (1023-a)*10000/a; //get the resistance of the sensor;
-        //console.log("Resistance: "+resistance);
-        var celsius_temperature = 1/(Math.log(resistance/10000)/B+1/298.15)-273.15;//convert to temperature via datasheet ;
-        //console.log("Celsius Temperature "+celsius_temperature); 
-        var fahrenheit_temperature = (celsius_temperature*(9/5))+35;
-        //console.log("Fahrenheit Temperature "+ fahrenheit_temperature);
-        socket.emit("message",fahrenheit_temperature);
-    },4000);
-}
