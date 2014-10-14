9cb905ea0068402148707ff1b05cfdb5c37c0a24
eashtian <eashtian@EASHTIAN-MOBL1.amr.corp.intel.com>
2014-10-13 17:12:32 -0700

    update project files

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-/*jslint node:true,vars:true,bitwise:true */
-/*global mraa */
+/*jslint node:true,vars:true,bitwise:true,unparam:true */
 
+/*jshint unused:true */
+
+/*
+The Local Temperature Node.js sample application distributed within Intel® XDK IoT Edition under the IoT with Node.js Projects project creation option showcases how to read analog data from a Grover Starter Kit Plus – IoT Intel® Edition Temperature Sensor, start a web server and communicate wirelessly using WebSockets.
+
+MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
+Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with port nanmes/numbering that match boards & with bindings to javascript & python.
+
+Steps for installing MRAA & UPM Library on Intel IoT Platform with IoTDevKit Linux* image
+Using a ssh client: 
+1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
+2. opkg update
+3. opkg upgrade
+
+Article: https://software.intel.com/en-us/html5/articles/iot-local-temperature-nodejs-and-html5-samples
+*/
 
 var B = 3975;
 var mraa = require("mraa");
@@ -9,7 +24,9 @@ var mraa = require("mraa");
 var myAnalogPin = new mraa.Aio(0);
 
 /*
-
+Function: startSensorWatch(socket)
+Parameters: socket - client communication channel
+Description: Read Temperature Sensor and send temperature in degrees of Fahrenheit every 4 seconds
 */
 function startSensorWatch(socket) {
     'use strict';
@@ -46,10 +63,10 @@ io.on('connection', function (socket) {
     console.log('a user connected');
     //Emits an event along with a message
     socket.emit('connected', 'Welcome');
-    
+
     //Start watching Sensors connected to Galileo board
     startSensorWatch(socket);
-    
+
     //Attach a 'disconnect' event handler to the socket
     socket.on('disconnect', function () {
         console.log('user disconnected');
@@ -60,4 +77,3 @@ http.listen(1337, function () {
     'use strict';
     console.log('listening on *:1337');
 });
-
